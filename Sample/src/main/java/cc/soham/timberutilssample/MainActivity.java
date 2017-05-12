c520e6e371d6b3210baf554b1732088f90436051
Soham Mondal <soham.mondal@gmail.com>
2017-05-12 17:18:43 +0530

    Added a bunch of TODOs

--- Sample/src/main/java/cc/soham/timberutilssample/MainActivity.java
+++ Sample/src/main/java/cc/soham/timberutilssample/MainActivity.java
@@ -6,6 +6,13 @@ import android.os.Bundle;
 import cc.soham.timberutils.TimberWrapper;
 import timber.log.Timber;
 
+/**
+ * TODO: add example for crashlytics
+ * TODO: add example for updating debug logging based on build types
+ * TODO: add example for updating file logging based on preferences
+ * TODO: add example of user explicitly disabling logging
+ * TODO: add example of zipping all files to share with customer care
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
